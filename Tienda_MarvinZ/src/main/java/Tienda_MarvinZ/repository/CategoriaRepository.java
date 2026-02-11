3a025cd8efe2cc0915787329b13c80192714a204
Marvin-017 <mzumbado50813@ufide.ac.cr>
2026-02-11 15:37:40 -0600

    Semana 3 del campus.actualizado
    avance de la semana 3 del campus, el primero se subio mal.

--- Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/repository/CategoriaRepository.java
+++ Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/repository/CategoriaRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 /**
  *
- * @author marvin
+ * @author josep
  */
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
     public List<Categoria> findByActivoTrue();
--- Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/service/CategoriaService.java
+++ Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/service/CategoriaService.java
@@ -26,3 +26,5 @@ public class CategoriaService {
         return categoriaRepository.findAll();
     }
 }
+
+
