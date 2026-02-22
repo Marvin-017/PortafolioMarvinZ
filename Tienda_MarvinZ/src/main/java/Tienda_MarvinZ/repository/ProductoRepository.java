e1655475a59c7de5de030dad935b2f1da7707925
Marvin-017 <mzumbado50813@ufide.ac.cr>
2026-02-22 01:28:15 -0600

    Avance semana 6
    Avance la de la filmina de la semana 6 completada con exito.

---
+++ Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/repository/ProductoRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tienda_MarvinZ.repository;
+
+import Tienda_MarvinZ.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+    public List<Producto> findByActivoTrue();
+}
+    
+
+
