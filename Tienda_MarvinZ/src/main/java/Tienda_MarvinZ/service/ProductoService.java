a6c9792ab70775837ead2c61d237829f89e07acc
Marvin-017 <mzumbado50813@ufide.ac.cr>
2026-02-27 23:18:08 -0600

    Avance de la semana 8 del campus
    Listo lo de las consultas. omito el archivo .json para que no haya conflicto

---
+++ Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/service/ProductoService.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda_MarvinZ.service;
+
+import Tienda_MarvinZ.domain.Producto;
+import Tienda_MarvinZ.repository.ProductoRepository;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.math.BigDecimal;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activo) {
+        if (activo) {
+            return productoRepository.findByActivoTrue();
+        }
+        return productoRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Producto> getProducto(Integer idProducto) {
+        return productoRepository.findById(idProducto);
+    }
+
+    @Transactional
+    public void save(Producto producto, MultipartFile imagenFile) {
+        producto = productoRepository.save(producto);
+
+        if (!imagenFile.isEmpty()) {
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(
+                        imagenFile,
+                        "producto",
+                        producto.getIdProducto()
+                );
+
+                producto.setRutaImagen(rutaImagen);
+                productoRepository.save(producto);
+
+            } catch (IOException e) {
+                // Podés loguearlo si querés
+            }
+        }
+    }
+
+    @Transactional
+    public void delete(Integer idProducto) {
+
+        if (!productoRepository.existsById(idProducto)) {
+            throw new IllegalArgumentException(
+                    "La producto con ID " + idProducto + " no existe."
+            );
+        }
+
+        try {
+            productoRepository.deleteById(idProducto);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException(
+                    "No se puede eliminar la producto. Tiene datos asociados.",
+                    e
+            );
+        }
+    }
+     @Transactional(readOnly = true)
+    public List<Producto> consultaDerivada(BigDecimal precioInf, BigDecimal precioSup) {
+        return productoRepository.findByPrecioBetweenOrderByPrecioAsc(precioInf, precioSup);
+    }
+     @Transactional(readOnly = true)
+    public List<Producto> consultaJPQL(BigDecimal precioInf, BigDecimal precioSup) {
+        return productoRepository.consultaJPQL(precioInf, precioSup);
+    }
+     @Transactional(readOnly = true)
+    public List<Producto> consultaSQL(BigDecimal precioInf, BigDecimal precioSup) {
+        return productoRepository.consultaSQL(precioInf, precioSup);
+    }
+}
