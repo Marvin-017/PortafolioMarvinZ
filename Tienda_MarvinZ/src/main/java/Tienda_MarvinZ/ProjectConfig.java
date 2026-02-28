f88e67969dc7ce1698ab5e022be1c0e43d915741
Marvin-017 <mzumbado50813@ufide.ac.cr>
2026-02-28 16:17:52 -0600

    Avance semana 10
    No se subieron todos los archivos en el commit anterior

--- Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/ProjectConfig.java
+++ Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/ProjectConfig.java
@@ -1,11 +1,20 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Tienda_MarvinZ;
 
-
+import java.util.Locale;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
@@ -34,9 +43,6 @@ public class ProjectConfig implements WebMvcConfigurer {
         resolver.setCheckExistence(true);
         return resolver;
     }
-<<<<<<< Updated upstream
-}
-=======
 
     @Bean
     public LocaleResolver localeResolver() {
@@ -69,4 +75,3 @@ public class ProjectConfig implements WebMvcConfigurer {
     
     
 }
->>>>>>> Stashed changes
