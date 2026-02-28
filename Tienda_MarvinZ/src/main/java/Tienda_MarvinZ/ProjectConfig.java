027fbf8ff6515fbcbc0656ecc16988e101517c1d
Marvin-017 <mzumbado50813@ufide.ac.cr>
2026-02-28 16:16:20 -0600

    Avance de la semana 10
    Completado el avance de semana 10, las rutas, URL y endpoints.

--- Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/ProjectConfig.java
+++ Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/ProjectConfig.java
@@ -34,4 +34,39 @@ public class ProjectConfig implements WebMvcConfigurer {
         resolver.setCheckExistence(true);
         return resolver;
     }
+<<<<<<< Updated upstream
 }
+=======
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+    
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    
+    
+}
+>>>>>>> Stashed changes
---
+++ Tienda_MarvinZ/src/main/java/Tienda_MarvinZ/repository/UsuarioRepository.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Tienda_MarvinZ.repository;
+
+/**
+ *
+ * @author marvi
+ */
+import Tienda_MarvinZ.domain.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    
+    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+    
+}
